4d363446f9d11f061544c5a141d0ca9a945a326b
amandena <manda.m.johnson@gmail.com>
2022-03-29 16:22:57 -0400

    wrote code in congrats component to make initial tests pass

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,18 @@
 
-export default function Congrats({ success }) {
-  return <div />
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congrats! You guessed the word!
+        </span>
+      </div>
+    )
+  } else {
+    return (
+      <div data-test='component-congrats'/>
+    )
+  }
 }
+
+export default Congrats
