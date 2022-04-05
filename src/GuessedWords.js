5d23a0793c90246fb44976f34704eb1f795358d4
amandena <manda.m.johnson@gmail.com>
2022-04-04 22:15:22 -0400

    set up prop types in guessed words component

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,7 +9,12 @@ const GuessedWords = (props) => {
 }
 
 GuessedWords.propTypes = {
-
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 }
 
 export default GuessedWords
