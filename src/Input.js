17984c7b32609a39066645786f12ade1ee566887
amandena <manda.m.johnson@gmail.com>
2022-04-12 22:48:08 -0400

    added currentGuess state and a form

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,24 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('')
   return (
     <div data-test='input-component'>
-
+      <form className='form-inline'>
+        <input 
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='enter guess'
+        />
+        <button
+          data-test='submit-button'
+          className='btn btn-primary mb-2'
+        >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
