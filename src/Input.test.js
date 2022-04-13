477387dab19805682190f26fb41267df5a7b30b3
amandena <manda.m.johnson@gmail.com>
2022-04-12 22:50:38 -0400

    wrote first test for form input field

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { shallow } from 'enzyme'
 import { findByTestAttr, checkProps } from '../test/testUtils'
 import Input from './Input'
@@ -19,4 +20,19 @@ test('renders without error', () => {
   const wrapper = setup()
   const component = findByTestAttr(wrapper, 'input-component')
   expect(component.length).toBe(1)
+})
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn()
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess])
+
+    const wrapper = setup()
+    const inputBox = findByTestAttr(wrapper, 'input-box')
+
+    const mockEvent = { target: { value: 'train' } }
+    inputBox.simulate('change', mockEvent)
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
+  })
 })
