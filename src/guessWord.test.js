fddeeee75275ccb49d3bc49c5812824a91b1eabe
amandena <manda.m.johnson@gmail.com>
2022-04-20 23:42:01 -0400

    wrote out more tests for guessed words

--- src/guessWord.test.js
+++ src/guessWord.test.js
@@ -31,9 +31,49 @@ describe('no words guessed', () => {
 })
 
 describe('some words guessed', () => {
-
+  let wrapper 
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1 }]
+    })
+  })
+  test('adds row to guessedWords table', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordNodes).toHaveLength(3)
+  })
 })
 
 describe('guess secret word', () => {
-  
+  let wrapper 
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1 }]
+    })
+  })
+  const inputBox = findByTestAttr(wrapper, 'submit-button')
+  const mockEvent = { target: { value: 'party' } }
+  inputBox.simulate('change', mockEvent)
+
+  const submitButton = findByTestAttr(wrapper, 'submit-button')
+  submitButton.simulate('click', { preventDefault() {} })
+
+  test('adds row to guessedWords table', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordNodes).toHaveLength(3)
+  })
+  test('displays congrats component', () => {
+    const congrats = findByTestAttr(wrapper, 'component-congrats')
+    expect(congrats.text().length).toBeGreaterThan(0)
+  })
+  test('does not display input component contents', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box')
+    expect(inputBox.exists()).toBe(false)
+
+    const submitButton = findByTestAttr(wrapper, 'submit-button')
+    expect(submitButton.exists()).toBe(false)
+  })
 })
