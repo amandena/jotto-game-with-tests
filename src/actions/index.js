ee05ddb43ac865f95ed58b61ed4a8cfdb816d0e3
amandena <manda.m.johnson@gmail.com>
2022-05-03 23:20:40 -0400

    completed guessWord fn in actions index to get tests passing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,24 @@
 import axios from 'axios'
+import { getLetterMatchCount } from '../helpers'
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD'
 }
 
-export const guessWord = (guessedWOrd) => {
+export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
+    const secretWord = getState().secretWord
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord)
 
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    })
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS })
+    }
   }
 }
 
