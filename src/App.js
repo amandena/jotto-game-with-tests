788aee58113a58536e1f2f1afe27ad12b532f12d
amandena <manda.m.johnson@gmail.com>
2022-05-05 19:31:54 -0400

    updated success and guessedWords variables with useSelector

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css'
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import Congrats from './Congrats'
 import GuessedWords from './GuessedWords'
 import Input from './Input'
 import { getSecretWord } from './actions'
 
 function App() {
-  const success = false 
+  const success = useSelector(state => state.success)
+  const guessedWords = useSelector(state => state.guessedWords)
   const secretWord = 'party'
-  const guessedWords = []
 
   useEffect(() => {
     getSecretWord()
